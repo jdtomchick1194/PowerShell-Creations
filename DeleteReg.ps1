9d26fcb5e6b15a4c06fc4463dca8adba2c95a2b8
jdtomchick1194 <114512142+jdtomchick1194@users.noreply.github.com>
2022-09-28 17:17:08 -0400

    re-uploaded. whoops

---
+++ DeleteReg.ps1
@@ -0,0 +1,18 @@
+#Author John Tomchick 09/20/2022
+#References: https://adamtheautomator.com/powershell-to-get-a-registry-value/#Getting_Registry_Keys_and_Values_with_Get-ChildItem, https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/remove-itemproperty?view=powershell-7.2
+#Create function to test if reg key and value exist
+Function Test-RegistryValue ($regkey, $name) {
+    if (Get-ItemProperty -Path $regkey -Name $name -ErrorAction Ignore) {
+        $true
+    } else {
+        $false
+    }
+}
+# Get reg item to delete 
+$path = Read-Host -Prompt "Please enter the full path to the registry key to delete"
+#Use function to test if key and value exist
+Test-RegistryValue $path
+#If exists, delete
+    if($true -ieq "True"){
+        Remove-ItemProperty -Path $path
+        }
